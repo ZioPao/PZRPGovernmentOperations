5783158de67eb18504ec568236f881235bfa1b4e
Pao <picchetti1998@gmail.com>
2023-05-22 16:21:55 +0200

    Changed ID contents

--- media/lua/client/UI/Forms/PZRPGovOps_IdentificationCard.lua
+++ media/lua/client/UI/Forms/PZRPGovOps_IdentificationCard.lua
@@ -3,8 +3,8 @@ local PZRPGovOps_IdentificationCard = {}
 PZRPGovOps_IdentificationCard.CreateChildren = function(instance, yOffset)
 	local xPadding = 10
 
-	instance.idform = ISLabel:new((instance.width - getTextManager():MeasureStringX(UIFont.Large, "Identification")) / 2, yOffset, 25, "Identification", 1, 1, 1, 1, UIFont.Large, true)
-	instance:addChild(instance.idform)
+	instance.mainLabel = ISLabel:new((instance.width - getTextManager():MeasureStringX(UIFont.Large, "Identification")) / 2, yOffset, 25, "Identification", 1, 1, 1, 1, UIFont.Large, true)
+	instance:addChild(instance.mainLabel)
 	yOffset = yOffset + 35
 
 	instance.fullNameEntry = ISTextEntryBox:new("Full Name", xPadding, yOffset, 150, 25)
@@ -20,22 +20,28 @@ PZRPGovOps_IdentificationCard.CreateChildren = function(instance, yOffset)
 	instance.birthdateEntry:setOnlyNumbers(false)
 	instance:addChild(instance.birthdateEntry)
 	yOffset = yOffset + 35
-	
-	instance.sexLabel = ISLabel:new(xPadding, yOffset, 25, "Sex", 1, 1, 1, 1, UIFont.Medium, true)
-	instance:addChild(instance.sexLabel)
-	yOffset = yOffset + 25
-	
-	instance.sexEntry = ISComboBox:new(xPadding, yOffset, 150, 25)
-	instance.sexEntry:initialise()
-	instance.sexEntry:addOption("M")
-	instance.sexEntry:addOption("F")
-	instance:addChild(instance.sexEntry)
+
+	instance.issuedEntry = ISTextEntryBox:new("Issued (MM/DD/YYYY)", xPadding, yOffset, 150, 25)
+	instance.issuedEntry:initialise()
+	instance.issuedEntry:instantiate()
+	instance.issuedEntry:setOnlyNumbers(false)
+	instance:addChild(instance.issuedEntry)
 	yOffset = yOffset + 35
-	
-	instance.eyeColorLabel = ISLabel:new(xPadding, yOffset, 25, "Eye Clr", 1, 1, 1, 1, UIFont.Medium, true)
-	instance:addChild(instance.eyeColorLabel)
-	yOffset = yOffset + 25
-	
+
+	instance.expirationEntry = ISTextEntryBox:new("Expiration (MM/DD/YYYY)", xPadding, yOffset, 150, 25)
+	instance.expirationEntry:initialise()
+	instance.expirationEntry:instantiate()
+	instance.expirationEntry:setOnlyNumbers(false)
+	instance:addChild(instance.expirationEntry)
+	yOffset = yOffset + 35
+
+	instance.heightEntry = ISTextEntryBox:new("Height (feet)", xPadding, yOffset, 150, 25)
+	instance.heightEntry:initialise()
+	instance.heightEntry:instantiate()
+	instance.heightEntry:setOnlyNumbers(false)
+	instance:addChild(instance.heightEntry)
+	yOffset = yOffset + 35
+
 	instance.eyeColorEntry = ISComboBox:new(xPadding, yOffset, 150, 25)
 	instance.eyeColorEntry:initialise()
 	instance.eyeColorEntry:addOption("Bl")
@@ -44,42 +50,17 @@ PZRPGovOps_IdentificationCard.CreateChildren = function(instance, yOffset)
 	instance.eyeColorEntry:addOption("Gy")
 	instance.eyeColorEntry:addOption("HET")
 	instance:addChild(instance.eyeColorEntry)
-	yOffset = yOffset + 35
-	
-	instance.hairColorLabel = ISLabel:new(xPadding, yOffset, 25, "Hair Clr", 1, 1, 1, 1, UIFont.Medium, true)
-	instance:addChild(instance.hairColorLabel)
-	yOffset = yOffset + 25
-	
-	instance.hairColorEntry = ISComboBox:new(xPadding, yOffset, 150, 25)
-	instance.hairColorEntry:initialise()
-	instance.hairColorEntry:addOption("Aub")
-	instance.hairColorEntry:addOption("Bld")
-	instance.hairColorEntry:addOption("Blk")
-	instance.hairColorEntry:addOption("Br")
-	instance.hairColorEntry:addOption("Gn")
-	instance.hairColorEntry:addOption("Rd")
-	instance.hairColorEntry:addOption("Wh")
-	instance:addChild(instance.hairColorEntry)
-	yOffset = yOffset + 35
-	
-	instance.heightEntry = ISTextEntryBox:new("Height (feet)", xPadding, yOffset, 150, 25)
-	instance.heightEntry:initialise()
-	instance.heightEntry:instantiate()
-	instance.heightEntry:setOnlyNumbers(false)
-	instance:addChild(instance.heightEntry)
-	yOffset = yOffset + 35
-	
-	instance.weightEntry = ISTextEntryBox:new("Weight (pounds)", xPadding, yOffset, 150, 25)
-	instance.weightEntry:initialise()
-	instance.weightEntry:instantiate()
-	instance.weightEntry:setOnlyNumbers(true)
-	instance:addChild(instance.weightEntry)
-
 end
 
 PZRPGovOps_IdentificationCard.Print = function(instance)
     local title = tostring(instance.fullNameEntry:getText()) .. " [ID]"
-    local contents = "Birth Date: " .. tostring(instance.birthdateEntry:getText()) .. "\nSex: " .. tostring(instance.sexEntry:getSelectedText()) .. "\nEye Clr: " .. tostring(instance.eyeColorEntry:getSelectedText()) .. "\nHair Clr: " .. tostring(instance.hairColorEntry:getSelectedText()) .. "\nHeight: " .. tostring(instance.heightEntry:getText()) .. "\nWeight: " .. tostring(instance.weightEntry:getText() .. " lbs")
+
+    local contents = "Birth Date: " .. tostring(instance.birthdateEntry:getText()) .. 
+		"\nIssued: " .. tostring(instance.issuedEntry:getSelectedText()) .. 
+		"\nExpires: " .. tostring(instance.expirationEntry:getSelectedText()) ..
+		"\nHeight: " .. tostring(instance.heightEntry:getSelectedText()) .. 
+		"\nEye Clr: " .. tostring(instance.eyeColorEntry:getSelectedText())
+
 	PZRP_GovOpsMain.PrintDocumentAndCopy("Base.IdentificationCard", title, contents)
 end
 
